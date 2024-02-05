21ffeae14740fe40a2fcd8257010f01786713b63
Tim Mairose <tmairose@gmail.com>
2024-02-04 18:00:00 -0700

    Fix Deploy to render issue

--- backend/db/migrations/20240126032222-create-booking.js
+++ backend/db/migrations/20240126032222-create-booking.js
@@ -1,5 +1,11 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Bookings', {
@@ -33,9 +39,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bookings');
+    options.tableName = "Bookings"
+    await queryInterface.dropTable(options);
   }
 };
